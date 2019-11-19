c8c39fc71778d3c3e89f9daf8f25b1161c672ec2
Steve Lamers <stevenlamers@gmail.com>
2019-11-19 08:12:49 -0600

    Update with Version 2.0 code
    Changed code for version 2.0

--- liat.c
+++ liat.c
@@ -1,21 +1,12 @@
-/* **********************************************************************************
-*
+/* ***************************************************************************
 * liat.c
-* 
 * liat - List It At the Top
-* 
 * Written by: Steven G. Lamers - COPYRIGHT 4-30-2019 - ALL RIGHTS RESERVED
-*
-* Version: 1.0
-*
+* Version: 2.0
 * History:
 *       1. First beta release - 04-30-2019
-*
 * Usage: liat [filename]
-*       
-*
-* **********************************************************************************
-*/
+* ************************************************************************* */
 
 #include <stdio.h>
 #include <signal.h>
@@ -23,122 +14,92 @@
 #include <string.h>
 #include <ncurses.h>
 
- /* TODO - add a switch to overwite and not scroll on updates */
-
-#define MAX_LINE_LENGTH 1024
-#define MAX_LINE_NUMBER 200
-
-char  screen[MAX_LINE_NUMBER][MAX_LINE_LENGTH];   /* The screen */
+#define MAX_LINE_LENGTH 400
+#define MAX_LINE_NUMBER 100
 
 void resizeHandler(int);
 
-int height = 0, width = 0;
+char  screen[MAX_LINE_NUMBER][MAX_LINE_LENGTH];   /* The screen */
+int   height = 0, width = 0;
 
 int main(int argc, char **argv)
 {
-    FILE  *fp       = NULL ;
-    int   x         = 0 ;
-    int   q         = 0 ;
-    char  buffer[MAX_LINE_LENGTH] ;   /* line buffer */
- 
-    /* if no arguments are passed use stdin */
-    if ( argc >= 2 ) {
+FILE  *fp       = NULL ;
+int   x         = 0 ;
+int   q         = 0 ;
+char  buffer[MAX_LINE_LENGTH] ;   /* line buffer */
 
-      /* Open the file */
-      fp = fopen(argv[1], "r");
-  
-      if ( fp == NULL )
-      {
-        /* Can't open file */
-        printf("%s can't open input file %s\n", argv[0], argv[1]);
-        exit(1);
-      }
-    }
+  signal(SIGWINCH, resizeHandler);
 
-    signal(SIGWINCH, resizeHandler);
+  initscr();
 
-    initscr();
-
-    scrollok(stdscr, TRUE );
+  scrollok(stdscr, TRUE );
    
-    getmaxyx(stdscr, height, width);
+  getmaxyx(stdscr, height, width);
 
-    if( ( height > MAX_LINE_NUMBER ) || ( width > MAX_LINE_LENGTH ) )
+  if( ( height > MAX_LINE_NUMBER ) || ( width > MAX_LINE_LENGTH ) )
+  {
+
+    /* move to 0,0  */
+    wmove(stdscr, 0, 0);
+
+    /* clear the line */
+    clrtoeol();          
+                      
+    /* print the error */
+    wprintw( stdscr, "Sorry, the screen height is %d  the width is %d : MAX_LINE_NUMBER is %d and MAX_LINE_LENGTH is %d \n", 
+              height,
+              width, 
+              MAX_LINE_NUMBER,
+              MAX_LINE_LENGTH 
+            );
+
+    /* Refresh the screen */
+    wrefresh(stdscr);
+
+    exit(1);
+    
+  }
+ 
+  while(1)
+  {
+
+    /* Get a line */ 
+    getnstr(buffer, MAX_LINE_LENGTH - 1 );
+              
+    /* push stack down */
+    for( q = height ; q > 0 ; q-- )
     {
+      strcpy( screen[ q ] , screen[ q - 1 ] );      
+    }
+          
+    /* Insert the line at the top of the stack */
+    strcpy( screen[ 0 ] , buffer);
 
-      /* move to 0,0  */
-      wmove(stdscr, 0, 0);
+    /* Print output */
+    for( x=0 ; x <= (height - 1);  x++ )
+    {
+                          
+      /* move to x,0  */
+      wmove(stdscr, x, 0);
 
       /* clear the line */
       clrtoeol();          
                       
-      /* print the error */
-      wprintw( stdscr, "Sorry, the screen height is %d  the width is %d : MAX_LINE_NUMBER is %d and MAX_LINE_LENGTH is %d \n", 
-                height,
-                width, 
-                MAX_LINE_NUMBER,
-                MAX_LINE_LENGTH 
-              );
-
+      /* print the line */
+      wprintw( stdscr, "%s" , screen[ x ] );
+            
       /* Refresh the screen */
       wrefresh(stdscr);
-
-      exit(1);
     }
+  }
 
+  endwin();
   
-    while(1)
-    {
-
-        if( fp != NULL )
-        {
-           if(fgets( buffer, MAX_LINE_LENGTH, fp) == NULL )   
-           {   
-             continue;
-           }
-
-        }
-        else 
-        { 
-            getstr(buffer);
-        }
-        
-            /* push stack down */
-            for( q = height ; q > 0 ; q-- )
-            {
-              strcpy( screen[ q ] , screen[ q - 1 ] );      
-            }
-          
-            /* Insert the line at the top of the stack */
-            strcpy( screen[ 0 ] , buffer );
-
-            /* Print output */
-            for( x=0 ; x <= (height - 1);  x++ )
-            {
-                          
-              /* move to x,0  */
-              wmove(stdscr, x, 0);
-
-              /* clear the line */
-              clrtoeol();          
-                      
-              /* print the line */
-              wprintw( stdscr, "%s" , screen[ x ] );
-            
-              /* Refresh the screen */
-              wrefresh(stdscr);
-            }
-    }
-
-    endwin();
-  
-    exit(0);
-
-
   exit(0);
 
-/* end main */
 }
+
 
 void resizeHandler(int sig)
 {
@@ -146,20 +107,22 @@ void resizeHandler(int sig)
 
     getmaxyx(stdscr, h, w);  /* get the new screen size */
 
-    height = h;
-    width  = w;
+    height = h; /* Set height to window height */
+    width  = w; /* Set width to window width */
 
     if( height > MAX_LINE_NUMBER  )
     {
       height = MAX_LINE_NUMBER ;
     }
-
     
     if( width > MAX_LINE_LENGTH )
     {
       width = MAX_LINE_LENGTH ;
     }
+    
+    /* Resize the window */
+    wresize(stdscr, height, width);
 
+    /* Refresh the screen */
+    wrefresh(stdscr);
 }
-
-
