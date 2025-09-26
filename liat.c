4149720a5c6e85c5163a27e493518db7c9237112
n9sgg <stevenlamers@gmail.com>
2025-09-25 20:35:43 -0500

    Changed to read from stdin instead of file

--- liat.c
+++ liat.c
@@ -6,13 +6,15 @@
 * History:
 *       1. First beta release - 04-30-2019
 *       2. Second release after testing the changes made from V1
-* Usage: liat [filename]
+*       3. Changed to read from stdin instead of file
+* Usage: echo "foo" | liat 
 * ************************************************************************* */
 
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <ncurses.h>
 
 #define MAX_LINE_LENGTH 400
@@ -25,12 +27,10 @@ int   height = 0, width = 0;
 
 int main(int argc, char **argv)
 {
-FILE  *fp       = NULL ;
+
 int   x         = 0 ;
 int   q         = 0 ;
-char  buffer[MAX_LINE_LENGTH] ;   /* line buffer */
-
-  signal(SIGWINCH, resizeHandler);
+char  buffer[MAX_LINE_LENGTH] ;   /* line buffer   /* Quit if no arguments are passed. */
 
   initscr();
 
@@ -40,13 +40,12 @@ char  buffer[MAX_LINE_LENGTH] ;   /* line buffer */
 
   if( ( height > MAX_LINE_NUMBER ) || ( width > MAX_LINE_LENGTH ) )
   {
-
     /* move to 0,0  */
     wmove(stdscr, 0, 0);
 
     /* clear the line */
-    clrtoeol();          
-                      
+    clrtoeol();
+
     /* print the error */
     wprintw( stdscr, "Sorry, the screen height is %d  the width is %d : MAX_LINE_NUMBER is %d and MAX_LINE_LENGTH is %d \n", 
               height,
@@ -54,76 +53,84 @@ char  buffer[MAX_LINE_LENGTH] ;   /* line buffer */
               MAX_LINE_NUMBER,
               MAX_LINE_LENGTH 
             );
-
+    
     /* Refresh the screen */
     wrefresh(stdscr);
-
-    exit(1);
     
+    exit(1);
   }
- 
+
   while(1)
   {
 
-    /* Get a line */ 
-    getnstr(buffer, MAX_LINE_LENGTH - 1 );
-              
-    /* push stack down */
-    for( q = height ; q > 0 ; q-- )
-    {
-      strcpy( screen[ q ] , screen[ q - 1 ] );      
-    }
-          
-    /* Insert the line at the top of the stack */
-    strcpy( screen[ 0 ] , buffer);
+    if (fgets(buffer, MAX_LINE_LENGTH - 1, stdin) != NULL ) {
 
-    /* Print output */
-    for( x=0 ; x <= (height - 1);  x++ )
-    {
-                          
-      /* move to x,0  */
-      wmove(stdscr, x, 0);
+      wmove(stdscr, height, 0 );
 
       /* clear the line */
-      clrtoeol();          
-                      
-      /* print the line */
-      wprintw( stdscr, "%s" , screen[ x ] );
-            
-      /* Refresh the screen */
-      wrefresh(stdscr);
-    }
-  }
+      clrtoeol();  
 
-  endwin();
-  
-  exit(0);
-
-}
-
-
-void resizeHandler(int sig)
-{
-    int h, w;
-
-    getmaxyx(stdscr, h, w);  /* get the new screen size */
-
-    height = h; /* Set height to window height */
-    width  = w; /* Set width to window width */
-
-    if( height > MAX_LINE_NUMBER  )
-    {
-      height = MAX_LINE_NUMBER ;
-    }
+    /* print the error */
+    wprintw( stdscr, "Sorry, the screen height is %d  the width is %d : MAX_LINE_NUMBER is %d and MAX_LINE_LENGTH is %d \n", 
+              height,
+              width, 
+              MAX_LINE_NUMBER,
+              MAX_LINE_LENGTH 
+            );
     
-    if( width > MAX_LINE_LENGTH )
-    {
-      width = MAX_LINE_LENGTH ;
-    }
-    
-    /* Resize the window */
-    wresize(stdscr, height, width);
-
     /* Refresh the screen */
     wrefresh(stdscr);
-}
+    
+    exit(1);
+  }
+
+  while(1)
+  {
+
+    if (fgets(buffer, MAX_LINE_LENGTH - 1, stdin) != NULL ) {
+
+      wmove(stdscr, height, 0 );
+
+      /* clear the line */
+      clrtoeol();  
+  
+      /* print the line */
+      wprintw( stdscr, "%s" , buffer );
+
+      /* Refresh the screen */
+      wrefresh(stdscr);
+  
+      /* push stack down */
+      for( q = height - 1; q > 0 ; q-- )
+      {
+        strcpy( screen[ q ] , screen[ q - 1 ] );      
+      }
+          
+      /* Insert the line at the top of the stack */
+      strcpy( screen[ 0 ] , buffer);
+
+      /* Print output */
+      for( x=0 ; x < (height - 1);  x++ )
+      {                         
+        /* move to x,0  */
+        wmove(stdscr, x, 0);
+
+        /* clear the current line */
+        clrtoeol();          
+                      
+        /* print the new line */
+        wprintw( stdscr, "%s" , screen[ x ] );
+            
+        /* Refresh the screen */
+        wrefresh(stdscr);
+      }  
+    } else {
+            usleep(100000);           // Sleep for 100 milliseconds
+    }
+  }
+  
+  endwin();
+
+  exit(0);
+
+} /* end of main () */
